c8e4163cd77a298730e5c3af8335499766ab14db
nonamme <lewandowskikamil@yahoo.co.uk>
2015-10-13 07:41:48 +0000

    Adding main routes

--- hulk/config/routes.rb
+++ hulk/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'hulk/index'
+
+  get 'hulk/projects'
+
+  get 'hulk/links'
+
+  get 'hulk/about'
+
+  get 'hulk/contacts'
+
+  get 'hulk/index'
+
   root 'hulk#index'
 end
